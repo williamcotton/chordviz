53042eaaa1299a9c125312bcb1802649f399a14f
William Cotton <williamcotton@gmail.com>
2021-11-23 16:14:59 -0500

    update image frame extraction script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # chordviz
+
+```
+conda activate tensorflow
+python image_frame.py capo_0_shape_G_1.mov
+```
--- image_frame.py
+++ image_frame.py
@@ -9,12 +9,18 @@ def split_filename(filename):
     return name, ext
 
 
-vidcap = cv2.VideoCapture(filename)
+def convert_to_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+input_filename = f'video_data/{filename}'
+vidcap = cv2.VideoCapture(input_filename)
 name, ext = split_filename(filename)
 success, image = vidcap.read()
 count = 1
 while success:
-    cv2.imwrite(f'image_data/{name}_frame_{count}.jpg', image)
+    output_filename = f'image_data/{name}_frame_{str(count).zfill(5)}.jpg'
+    res = cv2.imwrite(output_filename, convert_to_grayscale(image))
+    print('Saved image ', count, success, res, output_filename)
     success, image = vidcap.read()
-    print('Saved image ', count)
     count += 1
