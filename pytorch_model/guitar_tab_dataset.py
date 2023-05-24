3d2aea7ab9488a025cca9213853f6df17f2dc12c
William Cotton <williamcotton@gmail.com>
2023-05-24 12:18:47 -0500

    add data augmentations to training set

--- pytorch_model/guitar_tab_dataset.py
+++ pytorch_model/guitar_tab_dataset.py
@@ -1,20 +1,30 @@
 import torch
 from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image
+import numpy as np
 
 
 class GuitarTabDataset(Dataset):
-    def __init__(self, images, labels):
+    def __init__(self, images, labels, transform=None):
         self.images = images
         self.labels = labels
+        self.transform = transform
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
-        image = self.images[idx]
+        # The original images are grayscale with shape (128, 128)
+        image = self.images[idx].reshape(128, 128)
+        # Convert to PIL Image
+        image = Image.fromarray((image * 255).astype(np.uint8), mode="L")
         label = self.labels[idx]
 
-        # Convert the images and labels to PyTorch tensors.
-        return torch.tensor(image, dtype=torch.float32), torch.tensor(
-            label, dtype=torch.float32
-        )
+        if self.transform:
+            image = self.transform(image)
+
+        # Convert the image back to a tensor after the transform
+        image = transforms.ToTensor()(image)
+
+        return image, torch.tensor(label, dtype=torch.float32)
--- pytorch_model/training_validation_testing_split.py
+++ pytorch_model/training_validation_testing_split.py
@@ -1,8 +1,21 @@
 from sklearn.model_selection import train_test_split
 from preprocess import load_data
 from guitar_tab_dataset import GuitarTabDataset
+from torchvision import transforms
+from torch.utils.data import ConcatDataset
 
 image_data, label_data = load_data("image_data", "./labeler/labels.db")
+
+transform = transforms.Compose(
+    [
+        transforms.RandomRotation(20),  # rotate images
+        transforms.RandomResizedCrop(128, scale=(0.8, 1.2)),  # vary the scale
+        transforms.RandomAffine(
+            degrees=0,
+            translate=(0.2, 0.2),  # shift the image around vertically and horizontally
+        ),
+    ]
+)
 
 # Split the dataset into train, validation, and test sets (80% train, 10% validation, 10% test)
 train_images, temp_images, train_labels, temp_labels = train_test_split(
@@ -12,7 +25,17 @@ val_images, test_images, val_labels, test_labels = train_test_split(
     temp_images, temp_labels, test_size=0.5, random_state=42
 )
 
-# Use these splits to create PyTorch Dataset objects for each set.
-train_dataset = GuitarTabDataset(train_images, train_labels)
+## Use these splits to create PyTorch Dataset objects for each set.
+
+# Original dataset
+original_dataset = GuitarTabDataset(train_images, train_labels)
+
+# Augmented dataset
+augmented_dataset = GuitarTabDataset(train_images, train_labels, transform=transform)
+
+# Concatenate the original and augmented datasets
+train_dataset = ConcatDataset([original_dataset, augmented_dataset])
+
+# Validation and test datasets
 val_dataset = GuitarTabDataset(val_images, val_labels)
 test_dataset = GuitarTabDataset(test_images, test_labels)
